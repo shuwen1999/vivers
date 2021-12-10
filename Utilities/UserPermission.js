60e320fdbf76dfdaa52310be2a884ffa586c9861
shuwen1999 <70445867+shuwen1999@users.noreply.github.com>
2021-12-10 20:53:36 +0800

    image upload 1

---
+++ Utilities/UserPermission.js
@@ -0,0 +1,10 @@
+import Constants from 'expo-Constants';
+import * as Permission from 'expo-permissions'
+
+class UserPersmissions{
+    getCameraPermission = async () => {
+        if (Constants.platform.android){
+            const {status}
+        }
+    }
+}
